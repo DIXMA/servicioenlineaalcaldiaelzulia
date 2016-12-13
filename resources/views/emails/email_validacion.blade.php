a0883fa3e88365a7bdcb75ef9a29caa5f4a1946d
Laptop Asus_X550Z - ingdi <ingdiego.corts65@gmail.com>
2016-12-13 13:51:09 -0500

    Iniciando reacomodacion de envio de correo ahora con phpmailer

---
+++ resources/views/emails/email_validacion.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ingdi
+ * Date: 12/12/2016
+ * Time: 10:15 PM
+ */
---
+++ resources/views/indycom/admin/send_mail.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ingdi
+ * Date: 12/12/2016
+ * Time: 10:29 PM
+ */
